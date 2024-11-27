bcd303febe7db8808f4c03704496ff47942979d8
Vili <vili.trusheva@gmail.com>
2024-11-27 21:24:25 +0100

    Added login form validation

--- src/routes/auth/login.tsx
+++ src/routes/auth/login.tsx
@@ -1,9 +1,12 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import { getSession } from "~/common/get-session";
 import { Button } from "~/components/Button";
 import { Heading } from "~/components/Heading";
+import { Input } from "~/components/InputField";
 
 export const Route = createFileRoute("/auth/login")({
   component: LoginPage,
@@ -15,34 +18,46 @@ export const Route = createFileRoute("/auth/login")({
   },
 });
 
-type LoginFormValues = {
-  email: string;
-  password: string;
-};
+const LoginFormScheme = z.object({
+  email: z.string().min(1).email(),
+  password: z
+    .string()
+    .min(8, { message: "Password should be at least 8 characters" }),
+});
 
 function LoginPage() {
-  const { register, handleSubmit } = useForm<LoginFormValues>();
+  const form = useForm<z.infer<typeof LoginFormScheme>>({
+    resolver: zodResolver(LoginFormScheme),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
   return (
-    <div className="flex h-[calc(100vh-20rem)] items-center justify-center">
+    <div className="flex min-w-96 items-center justify-center">
       <div className="flex flex-col gap-2">
         <Heading variant="h2">Log Ind</Heading>
 
         <form
-          onSubmit={handleSubmit((data) => {
+          onSubmit={form.handleSubmit((data) => {
             console.log("hi from login button", data);
           })}
           className="flex flex-col gap-4"
         >
-          <input
-            type="email"
+          <Input
+            name="email"
             placeholder="E-mail"
-            {...register("email", { required: true })}
+            register={form.register}
+            type="email"
+            error={form.formState.errors.email}
           />
-          <input
+          <Input
+            name="password"
+            placeholder="Adgangskode"
+            register={form.register}
             type="password"
-            placeholder="password"
-            {...register("password", { required: true })}
+            error={form.formState.errors.password}
           />
           <Button variant="primary" size="md" type="submit">
             Log ind
